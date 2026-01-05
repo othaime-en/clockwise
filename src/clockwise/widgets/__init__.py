016b810ec513213488845f55f45f06270796d6ac
othaime-en <61498231+othaime-en@users.noreply.github.com>
2026-01-05 10:55:27 +0300

    feat: expose TimerWidget, StopwatchWidget and preset screens in __init__

---
+++ src/clockwise/widgets/__init__.py
@@ -0,0 +1,5 @@
+from .timer import TimerWidget
+from .stopwatch import StopwatchWidget
+from .preset_manager import PresetListScreen, NewTimerScreen
+
+__all__ = ["TimerWidget", "StopwatchWidget", "PresetListScreen", "NewTimerScreen"]
